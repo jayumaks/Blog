80bb3bf5fb493a27b0ec38e4b8ad559249d3716b
Efereyan Karen Simisola <thedevkaren@gmail.com>
2020-10-15 17:14:57 +0100

    Update port to be able to listen on heroku
    Signed-off-by: Efereyan Karen Simisola <thedevkaren@gmail.com>

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -64,6 +67,4 @@ app.post('/compose',function(req, res){
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(port);
